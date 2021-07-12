dcb62caa8af7d6306098f79f2d157f4f5a33642c
LeGoblin0 <legoblin@naver.com>
2021-07-13 05:39:37 +0900

    NPC 판별해서 대화창에 이름 띄우기
    NPC 판별해서 대화창에 이름 띄우기

--- Assets/02. Scripts/System/DialogManager.cs
+++ Assets/02. Scripts/System/DialogManager.cs
@@ -22,7 +22,7 @@ public class DialogManager : MonoBehaviour
             isAction = true;
             talkPanel.SetActive(true);
             scanObject = scanObj;
-            talk.SetMsg("대화문 테스트를 길게길게 써야지 왼쪽에서부터 텍스트 뜨는 속도를 조정하지");
+            talk.SetMsg("이 오브젝트는" + scanObject.name + "입니다.");
 
         }
         talkPanel.SetActive(isAction);
