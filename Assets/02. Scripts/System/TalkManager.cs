5cb76f2447be02c1ea3f5cb803ee43cf78836643
LeGoblin0 <legoblin@naver.com>
2021-07-21 23:25:11 +0900

    퀘스트 구현
    박스걸 -> 열쇠 -> 박스걸 순서로 말거는 퀘스트

--- Assets/02. Scripts/System/TalkManager.cs
+++ Assets/02. Scripts/System/TalkManager.cs
@@ -15,14 +15,28 @@ public class TalkManager : MonoBehaviour
 
     void GenerateData()
     {
-        talkData.Add(1000, new string[] { "1번 대화문,대화문대화문대화문", "2번 대화문 ,테스트를 계속" });
+        //Talk Data
+        //BoxGirl 1000
+        //Key 100
+        talkData.Add(1000, new string[] { "택배는 역시 로켓 배송이야", "택배 도착했을때가 제일 짜릿해" });
+        talkData.Add(2000, new string[] {"수상하게 생긴 열쇠다."});
 
-        talkData.Add(10 + 1000, new string[] { "퀘스트 대화문 테스트", "퀘스트 대화문 테스트 2" });
+        //Quest Talk
+        talkData.Add(10 + 1000, new string[] { "안녕? 난 박스걸이야", "내가 박스안에 갇혀버려서 부탁하나만 해도 될까?","저 쪽에 있는 열쇠좀 주워줄래?" });
+        talkData.Add(20 + 2000, new string[] { "수상한 열쇠를 주웠다."});
+        talkData.Add(21 + 1000, new string[] { "열쇠 가져다 줘서 고마워!" });
 
     }
     public string GetTalk(int id, int talkIndex)
     {
-        if(talkIndex == talkData[id].Length)
+        if (!talkData.ContainsKey(id))
+        {
+            if (!talkData.ContainsKey(id - id % 10))
+                return GetTalk(id - id % 100, talkIndex);
+            else
+                 return GetTalk(id - id % 10, talkIndex);
+        }
+        if (talkIndex == talkData[id].Length)
             return null;
         else
             return talkData[id][talkIndex];
