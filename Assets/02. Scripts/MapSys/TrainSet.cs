e56e2e1feba19326eb654a5a31d100addb8efdd3
NaDeNeunNa <75741403+NaDeNeunNa@users.noreply.github.com>
2021-08-26 12:24:52 +0900

    오류 수정

--- Assets/02. Scripts/MapSys/TrainSet.cs
+++ Assets/02. Scripts/MapSys/TrainSet.cs
@@ -6,10 +6,21 @@ public class TrainSet : MonoBehaviour
 {
     public Vector2[] GoTT;
     public bool Stop = false;
+    public bool YSet = true;
+
+    private void Awake()
+    {
+        if (YSet)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, 6);
+        }
+    }
     void Start()
     {
         gameObject.layer = 27;
         gameObject.tag = "TrainSet";
+
+
     }
 
     // Update is called once per frame
